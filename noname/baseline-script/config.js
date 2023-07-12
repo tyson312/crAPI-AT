2d006a05a46efbe2bab0a2fb1690f63aff6c2cb4
tyson312 <tyson312@localhost>
2023-07-12 16:12:27 -0700

    update baseline config.js to use localhost urls

--- noname/baseline-script/config.js
+++ noname/baseline-script/config.js
@@ -1,6 +1,6 @@
 export default {
-  crapi: 'CRAPI_HOST',
-  mailhog: 'MAILHOG_HOST',
+	crapi: 'localhost:8888',
+  mailhog: 'localhost:8025',
   vinRegex: /\b[(A-Z|0-9)]{17}\b/gm,
   pinRegex: />(\d{4})<\/font>/,
   usersToSimulate: parseInt(process.env.USERS_TO_SIMULATE ?? 100),
